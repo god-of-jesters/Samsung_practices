c46d29d9d2f781e15923a55e33366cb0e9c66c40
god_of_jesters <ryzovdima@gmail.com>
2024-10-17 14:55:47 +0300

    xfkg

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,9 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.math.abs
+import kotlin.math.max
 
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    var k = readln().toInt()
+    var x = readln().toInt()
+    var y = readln().toInt()
+    println(max(max(k - max(x, y), 2 - abs(x - y)), 0))
 }
