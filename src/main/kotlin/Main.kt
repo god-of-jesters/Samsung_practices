259f78209e52d04e7dbd530ffbfb387553151e5e
god_of_jesters <ryzovdima@gmail.com>
2024-10-17 14:56:12 +0300

    xfkg

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import kotlin.math.abs
 import kotlin.math.max
 
-fun main(args: Array<String>) {
-    var k = readln().toInt()
-    var x = readln().toInt()
-    var y = readln().toInt()
+fun main() {
+    val k = readln().toInt()
+    val x = readln().toInt()
+    val y = readln().toInt()
     println(max(max(k - max(x, y), 2 - abs(x - y)), 0))
 }
